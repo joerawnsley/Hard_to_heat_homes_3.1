40c46e391be30d456c47185d8f8d2b162c93508d
Liam <liam.blackburn@madetech.com>
2025-12-15 17:12:57 +0000

    amended test suite for bristol filtering. Updated os_dummy_data to include limited bristol properties for test suite. Added test suite to check None returns empty array for filter function.

--- tests/test_filter_properties_by_council.py
+++ tests/test_filter_properties_by_council.py
@@ -1,4 +1,4 @@
-from src.council_data_utils import filter_properties_by_council_code
+from src.council_data_utils import filter_properties_by_council_code, load_uprn_to_council
 from src.utils import get_properties_from_os
 import json
 from src.property import Property
@@ -6,7 +6,10 @@ from src.property import Property
 with open("tests/test_data/os_dummy_data.json") as data:
     os_dummy_data = json.load(data)
     
-array_of_buildings = os_dummy_data["features"]
+array_of_buildings = []
+for collection in os_dummy_data:
+    array_of_buildings.extend(collection["features"])
+
 props = get_properties_from_os(array_of_buildings)
 dummy_uprn = 10090058306
 dummy_prop = Property(dummy_uprn)
@@ -17,7 +20,7 @@ def test_filter_properties_in_elmbridge():
     elmbridge_council_code = "E07000207"
     filtered_props = filter_properties_by_council_code(elmbridge_council_code, props)
 
-    assert filtered_props == props[:4]
+    assert len(filtered_props) == 4
 
 def test_filter_properties_in_east_hampshire():
     east_hampshire_council_code = "E07000085"
@@ -31,7 +34,14 @@ def test_filter_properties_in_tunbridge_wells():
 
     assert filtered_props == [dummy_prop]
 
-def test_filter_properties_in_None():
+def test_filter_properties_in_bristol():
+    bristol_council_code = "E06000023"
+    filtered_props = filter_properties_by_council_code(bristol_council_code, props)
+    uprn_to_council_dict = load_uprn_to_council(bristol_council_code)
+
+    assert all(uprn_to_council_dict[str(prop.uprn)] == bristol_council_code for prop in filtered_props)
+
+def test_filter_properties_for_():
     filtered_props = filter_properties_by_council_code(None, props)
 
     assert filtered_props == []    
