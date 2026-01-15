d6a44a0266ea1174fe40d0d62ebb409ed8186d8d
Etido Ukpong <etido.ukpong@madetech.com>
2026-01-15 15:11:17 +0000

    remove old filter properties javascript file

--- static/javascripts/filter-void-properties.js
+++
@@ -1,5 +0,0 @@
-function filterVoidProperties(arrayOfProperties) {
-    console.log(arrayOfProperties)
-    const filtered = arrayOfProperties.filter((prop) => prop.void == true)
-    return filtered
-}
