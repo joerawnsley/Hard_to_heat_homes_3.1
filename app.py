e2f6b869ae4e70604621d9d6b67246fff5ac1a43
Etido Ukpong <etido.ukpong@madetech.com>
2026-01-15 14:36:25 +0000

    refactor login route and remove unused import statements

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, session
-from src.utils import get_properties_from_os, get_attributes_from_epc, remove_blank_addresses, match_property_to_ccod
+from flask import Flask, render_template
 from src.variables import OS_KEY
-from src.council_data_utils import get_bbox_for_council_code, filter_properties_by_council_code
-from src.generate_heat_map import generate_heat_map
-from src.building_collection import BuildingCollection
 
 app = Flask(__name__)
 
@@ -14,31 +10,7 @@ app.secret_key = os.getenv("SESSION_SECRET_KEY")
 
 @app.route("/")
 def login():
-    council_code = "E06000023"
-    council_bbox = get_bbox_for_council_code(council_code)
-    return set_property_data(council_code, council_bbox)
-
-
-def set_property_data(council_code, council_bbox):
-    
-    list_of_buildings = BuildingCollection(council_bbox, 1).produce_list()
-    
-    global properties
-    properties = get_properties_from_os(list_of_buildings)
-    properties = filter_properties_by_council_code(council_code, properties)
-    properties = get_attributes_from_epc(properties)
-    properties = remove_blank_addresses(properties)
-
-    for i in range(len(properties)):
-        properties[i].calculate_score()
-
-    target_csv_path = 'data/TEST_BRISTOL_CCOD_DEC_2025.csv'
-    
-    for prop in properties:
-        match_property_to_ccod(target_csv_path, prop)
-    
-    heat_map = generate_heat_map(council_code)
-    return render_template("home.html", properties=properties, key=OS_KEY, heat_map=heat_map)
+    return render_template("home.html", key=OS_KEY)
 
 @app.route("/<int:uprn>")
 def property(uprn):
